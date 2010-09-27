3b0e8583725b48695eecb0c58a234fe290120dad
gurevichae <gurevichae@fb846ec2-02f3-2caf-f026-174acf40c2a8>
2010-09-27 21:15:16 +0000

    Небольшие правки. Логирование теперь выполняется вне класса, отдельными методами.

--- src/Logger.h
+++ src/Logger.h
@@ -7,30 +7,23 @@
 
 const int LOGGER_FILENAME_MAX = 256;
 
-#define LOG_OPEN(filename) g_Logger = new Logger(filename)
-#define LOG_CLOSE()        delete g_Logger
+#define LOG_OPEN(filename) LoggerOpen(filename)
+#define LOG_CLOSE()        LoggerClose()
 
 #if defined(_MSC_VER)
-#define LOG_DEBUG(str,...) g_Logger->write("%s:%s:%d DEBUG " str, __FILE__, __FUNCTION__, __LINE__, ##__VA_ARGS__)
-#define LOG_ERROR(str,...) g_Logger->write("%s:%s:%d ERROR " str, __FILE__, __FUNCTION__, __LINE__, ##__VA_ARGS__)
+#define LOG_DEBUG(str,...) LoggerWrite("%s:%s:%d DEBUG " str, __FILE__, __FUNCTION__, __LINE__, ##__VA_ARGS__)
+#define LOG_ERROR(str,...) LoggerWrite("%s:%s:%d ERROR " str, __FILE__, __FUNCTION__, __LINE__, ##__VA_ARGS__)
 #elif defined(__MINGW32__)
-#define LOG_DEBUG(str,...) g_Logger->write("%s:%s:%d DEBUG " str, __FILE__, __func__, __LINE__, ##__VA_ARGS__)
-#define LOG_ERROR(str,...) g_Logger->write("%s:%s:%d ERROR " str, __FILE__, __func__, __LINE__, ##__VA_ARGS__)
+#define LOG_DEBUG(str,...) LoggerWrite("%s:%s:%d DEBUG " str, __FILE__, __func__, __LINE__, ##__VA_ARGS__)
+#define LOG_ERROR(str,...) LoggerWrite("%s:%s:%d ERROR " str, __FILE__, __func__, __LINE__, ##__VA_ARGS__)
 #endif
 
 #define ASSERT(x)          assert(x)
 
-class Logger
-{
-public:
-	Logger(const char *fileName);
+void LoggerOpen(const char *fileName);
 
-	void write(const char *format, ...) const;
+void LoggerClose();
 
-private:
-	char m_fileName[LOGGER_FILENAME_MAX];
-};
-
-extern Logger *g_Logger;
+void LoggerWrite(const char *format, ...);
 
 #endif /* LOGGER_H */
