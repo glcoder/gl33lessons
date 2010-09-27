50da0cd2f443d9de563b9fbda6fd1345a9a7b11c
gurevichae <gurevichae@fb846ec2-02f3-2caf-f026-174acf40c2a8>
2010-09-27 16:30:15 +0000

    Стабильная ветка первого урока.

---
+++ branches/lesson01/src/OpenGL.cpp
@@ -0,0 +1,3 @@
+#include "OpenGL.h"
+
+GLenum g_OpenGLError = GL_NO_ERROR;
---
+++ branches/lesson01/src/common.h
@@ -0,0 +1,25 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+#include <cstdio>
+#include <cstdlib>
+#include <cstdarg>
+#include <cstring>
+#include <cassert>
+
+#if defined(_MSC_VER)
+typedef __int8           int8_t;
+typedef unsigned __int8  uint8_t;
+typedef __int16          int16_t;
+typedef unsigned __int16 uint16_t;
+typedef __int32          int32_t;
+typedef unsigned __int32 uint32_t;
+#elif defined(__MINGW32__)
+#include <stdint.h>
+#else
+#error You must compile this code with MSVC or MinGW!
+#endif
+
+#include "Logger.h"
+
+#endif /* COMMON_H */
