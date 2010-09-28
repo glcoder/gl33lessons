c0282670e5a24bf0aa6b75f1a3bf4386540d61d3
gurevichae <gurevichae@fb846ec2-02f3-2caf-f026-174acf40c2a8>
2010-09-28 07:40:07 +0000

    Убран уласс GLWindow, код стал портируемо-френдли

--- trunk/src/GLWindow.h
+++ trunk/src/GLWindow.h
@@ -1,52 +1,27 @@
 #ifndef GLWINDOW_H
 #define GLWINDOW_H
 
-#define WIN32_LEAN_AND_MEAN 1
-#include <windows.h>
-
 #include "common.h"
 #include "OpenGL.h"
 
-const char GLWINDOW_CLASS_NAME[] = "GLWindow_class";
-const char GLWINDOW_PROP_NAME[]  = "GLWindow_prop";
-
-// окно с поддержкой OpenGL
-class GLWindow
+// структура для хранения стейта окна GLWindow
+struct GLWindow
 {
-public:
-	typedef void (*pRenderProc)(GLWindow *);
+	// размеры окна
+	int width;
+	int height;
 
-	GLWindow();
-	~GLWindow();
-
-	// создание окна
-	bool create(const char *title, int width, int height, bool fullScreen = false);
-
-	// установка размеров окна и полноэкранного режима
-	void setSize(int width, int height, bool fullScreen = false);
-
-	// установка процедуры рендеринга
-	void setRenderProc(pRenderProc renderProc);
-
-	// основной цикл окна
-	int mainLoop();
-
-private:
-	// обработчик сообщений окна
-	LRESULT CALLBACK windowProc(UINT msg, WPARAM wParam, LPARAM lParam);
-
-	// заглушка для вызова windowProc
-	static LRESULT CALLBACK sWindowProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
-
-	HINSTANCE m_hInstance;
-	HWND      m_hWnd;
-	HDC       m_hDC;
-	HGLRC     m_hRC;
-
-	int  m_width, m_height;
-	bool m_fullScreen, m_active, m_running;
-
-	pRenderProc m_renderProc;
+	// флаги состояние
+	bool fullScreen;
+	bool active;
+	bool running;
 };
+
+bool GLWindowCreate(const char *title, int width, int height, bool fullScreen = false);
+void GLWindowDestroy();
+
+void GLWindowSetSize(int width, int height, bool fullScreen = false);
+
+int GLWindowMainLoop();
 
 #endif /* GLWINDOW_H */
