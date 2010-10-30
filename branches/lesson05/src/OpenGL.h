b421c48a5dedbd540e3fc00988fc6b9edf4b5052
gurevichae <gurevichae@fb846ec2-02f3-2caf-f026-174acf40c2a8>
2010-10-30 10:44:01 +0000

    Убрал загрузку лишних OpenGL-функций из пятого урока.

--- branches/lesson05/src/OpenGL.h
+++ branches/lesson05/src/OpenGL.h
@@ -77,11 +77,5 @@ extern PFNGLUNIFORM1IPROC          glUniform1i;
 extern PFNGLUNIFORM1FVPROC         glUniform1fv;
 extern PFNGLUNIFORM3FVPROC         glUniform3fv;
 extern PFNGLUNIFORM4FVPROC         glUniform4fv;
-// FBO
-extern PFNGLBINDFRAMEBUFFERPROC        glBindFramebuffer;
-extern PFNGLDELETEFRAMEBUFFERSPROC     glDeleteFramebuffers;
-extern PFNGLGENFRAMEBUFFERSPROC        glGenFramebuffers;
-extern PFNGLCHECKFRAMEBUFFERSTATUSPROC glCheckFramebufferStatus;
-extern PFNGLFRAMEBUFFERTEXTURE2DPROC   glFramebufferTexture2D;
 
 #endif /* OPENGL_H */
